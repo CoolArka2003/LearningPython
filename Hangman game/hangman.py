3336cbb3402457793edab3926684a79317f58c61
Arka Mukherjee <129255079+CoolArka2003@users.noreply.github.com>
2024-03-26 15:39:31 +0530

    Source code and game demo

---
+++ Hangman game/hangman.py
@@ -0,0 +1,56 @@
+import random
+from hangman_art import stages, logo
+from words_database import words_list
+
+print(logo)
+print("\n\nWelcome to Hangman")
+
+while True:
+    word=words_list[random.randint(0,199)].lower()
+    guessed_word=''
+    print(word)
+
+    for i in range(len(word)):
+        guessed_word+='_'
+    print(guessed_word)
+    guess=list(guessed_word)
+
+    flag=True
+    hang_count=0
+    while hang_count <= 6:
+        g = input("Enter your guess ").lower()
+
+        if g in guess:
+            print(f"You have already guessed {g}")
+        for i in range(len(word)):
+            if g == word[i]:
+                guess[i]=g
+                flag=False
+        for i in range(len(guess)):
+            print(guess[i],end="")
+        print()
+        if flag==True:
+            hang_count+=1
+            print("That's not in the word. You lose a life.")
+            print(stages[hang_count])
+        if hang_count == 6:
+            print("Game over")
+            break
+        winner=True
+        for i in range(len(guess)):
+            if (guess[i]=='_'):
+                winner=False
+        if winner:
+            print("You won")
+            break
+        flag=True
+    while True:
+        answer = str(input('Run again? (y/n): ').lower())
+        if answer in ('y', 'n'):
+            break
+        print("invalid input.")
+    if answer == 'y':
+        continue
+    else:
+        print("Goodbye")
+        break
